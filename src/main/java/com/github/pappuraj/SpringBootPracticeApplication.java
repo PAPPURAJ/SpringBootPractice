592c241379a3bb2896fb5f98fd3ba3d854c06b12
PAPPURAJ <pappuraj.duet@gmail.com>
2023-07-18 11:56:58 +0600

    JPA CRUD Operation

--- src/main/java/com/github/pappuraj/SpringBootPracticeApplication.java
+++ src/main/java/com/github/pappuraj/SpringBootPracticeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.github.pappuraj.jpa.MyJPA;
 import com.github.pappuraj.jpa.Students;
 import com.github.pappuraj.jpa.UserRepository;
 
@@ -15,44 +16,13 @@ public class SpringBootPracticeApplication {
 	public static void main(String[] args) {
 		ApplicationContext context= SpringApplication.run(SpringBootPracticeApplication.class, args);
 		
-		UserRepository userRepository=context.getBean(UserRepository.class);
+		MyJPA myJPA=new MyJPA(context);
 		
-		
-//		//insert
-
-		
-		
-		
-		
-		//Fetch one record
-//		Optional<Students> stdOpt = userRepository.findById(1);
-//		Students std=stdOpt.get();
-//		System.out.println(std);
-		
-		
-		
-		
-		
-		
-		//update
-//		Optional<Students> stdOpt = userRepository.findById(1);
-//		Students std=stdOpt.get();
-//		std.setName("Shayeri");;
-//		userRepository.save(std);
-//		System.out.println(stdOpt.get());
-		
-		
-		//Fetch all
-//		Iterable<Students> findAll = userRepository.findAll();
-//		findAll.forEach(student->{
-//			System.out.println(student);
-//		});
-		
-		
-		
-		//delete
-//		userRepository.deleteById(102);;
-		
+		//myJPA.insert();
+		//myJPA.view(0);
+		myJPA.viewAll();
+		//myJPA.delete(0);
+			
 		
 		
 	}
