423e13c3d2e06b6372517bef648f9c5476de8317
PAPPURAJ <pappuraj.duet@gmail.com>
2023-07-19 15:07:26 +0600

    File save to local storage using API

--- src/main/java/com/github/pappuraj/MyRestController.java
+++ src/main/java/com/github/pappuraj/MyRestController.java
@@ -1,5 +1,9 @@
 package com.github.pappuraj;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,8 +19,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.github.pappuraj.jpa.MyJPA;
 import com.github.pappuraj.jpa.Students;
@@ -57,6 +63,19 @@ public class MyRestController {
 	@DeleteMapping(path = "/student/delete/{id}")
 	public void deleteStudent(@PathVariable("id") int id) {
 		userRepository.deleteById(id);
+	}
+	
+	@PostMapping("/upload") //For file operation
+	public String upFile(@RequestParam("_file") MultipartFile file) {
+		
+		try {
+			Files.copy(file.getInputStream(), Path.of("C:\\Users\\Hp ZBooK 15u G5\\Documents\\SpringWorkspace\\SpringBootPractice\\src\\main\\java\\resource\\"+file.getOriginalFilename()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return file.getOriginalFilename()+" Size:"+file.getSize()/1024+"KB";
 	}
 
 }
